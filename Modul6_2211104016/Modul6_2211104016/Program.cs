3e1848c5f2c9af3610dd193bd20c3fa1e58c9a8f
Ridham2345 <idhamcholidrdn@gmail.con>
2025-03-21 07:22:19 +0700

    menambahkan dua class SayaTubeVideo dan SayaTubeuser

--- Modul6_2211104016/Modul6_2211104016/Program.cs
+++ Modul6_2211104016/Modul6_2211104016/Program.cs
@@ -6,10 +6,102 @@ using System.Threading.Tasks;
 
 namespace Modul6_2211104016
 {
+    public class SayaTubeVideo
+    {
+        private int id;
+        private string title;
+        private int playCount;
+
+        public SayaTubeVideo(string title)
+        {
+            Random random = new Random();
+            this.id = random.Next(10000, 99999);
+            this.title = title;
+            this.playCount = 0;
+        }
+
+        public void IncreasePlayCount(int count)
+        {
+            playCount += count;
+        }
+
+        public void PrintVideoDetails()
+        {
+            Console.WriteLine($"ID: {id}");
+            Console.WriteLine($"Title: {title}");
+            Console.WriteLine($"Play Count: {playCount}");
+        }
+
+        public int GetPlayCount()
+        {
+            return playCount;
+        }
+
+        public string GetTitle()
+        {
+            return title;
+        }
+    }
+
+    public class SayaTubeUser
+    {
+        private int id;
+        private string username;
+        private List<SayaTubeVideo> uploadedVideos;
+
+        public SayaTubeUser(string username)
+        {
+            Random random = new Random();
+            this.id = random.Next(10000, 99999);
+            this.username = username;
+            this.uploadedVideos = new List<SayaTubeVideo>();
+        }
+
+        public void AddVideo(SayaTubeVideo video)
+        {
+            uploadedVideos.Add(video);
+        }
+
+        public int GetTotalVideoPlayCount()
+        {
+            int totalPlayCount = 0;
+            foreach (var video in uploadedVideos)
+            {
+                totalPlayCount += video.GetPlayCount();
+            }
+            return totalPlayCount;
+        }
+
+        public void PrintAllVideoPlaycount()
+        {
+            Console.WriteLine($"User: {username}");
+            for (int i = 0; i < uploadedVideos.Count; i++)
+            {
+                Console.WriteLine($"Video {i + 1} judul: {uploadedVideos[i].GetTitle()}");
+            }
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            SayaTubeUser user = new SayaTubeUser("Arxc Bandit");
+
+            string[] hackerMovies = {
+                "The Matrix", "Hackers", "WarGames", "Sneakers", "Swordfish",
+                "Blackhat", "The Girl with the Dragon Tattoo", "Mr.Robot", "Tron", "Who Am I"
+            };
+
+            foreach (string movie in hackerMovies)
+            {
+                SayaTubeVideo video = new SayaTubeVideo($"Review Film {movie} oleh Arxc Bandit");
+                video.IncreasePlayCount(new Random().Next(100, 1000));
+                user.AddVideo(video);
+            }
+
+            user.PrintAllVideoPlaycount();
+            Console.WriteLine($"Total Play Count: {user.GetTotalVideoPlayCount()}");
         }
     }
 }
